bc165a56993a5a0440a9b1a135bed5d20b73f05a
chaddlong <67989254+chaddlong@users.noreply.github.com>
2022-12-07 15:17:22 +0100

    Add files via upload

---
+++ data_handler.py
@@ -0,0 +1,41 @@
+import data_repo
+import csv
+import math
+
+#class DataHandler:
+    #def __init__(self, csv_path):
+        #self.data_repo = data_repo
+    #def get_air_quality(coord, time_stamp):
+        #lat, long = coord
+        #air_quality_list = []
+        #for i in range(0,3):
+            #air_quality_list.append(calculate_mean_air_quality(i, time_stamp, (lat,long)))
+        
+        #total_air_quality = max(air_quality_list)
+
+        #return (total_air_quality, air_quality_list)
+    
+    #sensor_file = open('Sensors.csv')
+    #csvreader = csv.reader(sensor_file, delimiter=";")
+    #sensors_id = []
+    
+    #with open('Sensors.csv') as sensor_file:
+        #sensor_csv = csv.DictReader(sensor_file)
+        #for sid in sensor_csv:
+            #if (sqrt() <= )
+
+    #def haversine(theta):
+        #return math.sin(theta/2)**2
+    #def distance((, (lat_2,long_2)):
+        #return 12742*math.asin(sqrt(math.sin((lat_2-lat_1)/2)**2 + math.cos(lat_1)*math.cos(lat_2)*math.sin((long_2-long_1)/2)**2))
+    
+
+
+    #def calc_mean_air_quality(pollutant, timestamp, (float_1, float_2)):
+        #pollutant_values = data_repo.getSensorData(pollutant, timestamp, (float_1, float_2))
+        ###
+
+
+
+
+
